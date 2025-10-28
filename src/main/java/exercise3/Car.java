5e34feaafd1de7ba8563ddfc8ed576544f3dbcda
eneac <cicolecchiae@bzz.ch>
2025-10-28 10:48:55 +0100

    uebung 3

--- src/main/java/exercise3/Car.java
+++ src/main/java/exercise3/Car.java
@@ -1,26 +1,51 @@
 package exercise3;
 
 public class Car {
-  public int fuelLevel;
-  public int mileage;
+  public double fuelLevel;
+  public double mileage;
   public String licensePlate;
-  public String fuelConsumption;
-  public String fuelCapacity;
+  public double fuelConsumption;
+  public double fuelCapacity;
 
   public Car(String zh420, int i, int i1, double v, int i2) {
+      licensePlate = zh420;
+      fuelLevel = i2;
+      mileage = i;
+      fuelConsumption = v;
+      fuelCapacity = i1;
+
   }
 
-  //declare vars
 
-  //Create constructor
 
   public void drive(int distance) {
-    //do stuff
+
+
+      if (fuelLevel - distance * fuelConsumption < 0) {
+          double maxDistance = fuelLevel / fuelConsumption;
+          fuelLevel = 0.0;
+          mileage += (double) Math.round(maxDistance * 100) / 100;
+      } else {
+          mileage += distance;
+          fuelLevel =  fuelLevel - distance * fuelConsumption;
+      }
   }
 
   public void refuel(int amount) {
-    //do stuff
+      if (fuelLevel + amount > fuelCapacity) {
+          fuelLevel = fuelCapacity;
+      } else {
+         fuelLevel = fuelLevel + amount;
+      }
   }
 
-  //add toString
+  public String toString() {
+      return "Car{" +
+              "licensePlate='" + licensePlate + '\'' +
+              ", mileage=" + mileage +
+              ", fuelConsumption=" + fuelConsumption +
+              ", fuelCapacity=" + fuelCapacity +
+              ", fuelLevel=" + fuelLevel +
+              '}';
+  }
 }
--- src/main/java/exercise3/Main.java
+++ src/main/java/exercise3/Main.java
@@ -2,7 +2,7 @@ package exercise3;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: Create Car Object
+
 
 
     }
