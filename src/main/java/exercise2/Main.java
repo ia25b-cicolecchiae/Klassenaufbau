61655df704a78deac3516b686342652c8057ef86
eneac <cicolecchiae@bzz.ch>
2025-10-28 09:29:00 +0100

    uebung 2

--- src/main/java/exercise2/Main.java
+++ src/main/java/exercise2/Main.java
@@ -2,6 +2,7 @@ package exercise2;
 
 public class Main {
     public static void main(String[] args) {
-        //Main Method
+        Patient patient = new Patient("Matteo", "Bürki", 92, 2.02, 230, "Male");
+        System.out.println(patient);
     }
 }
--- src/main/java/exercise2/Patient.java
+++ src/main/java/exercise2/Patient.java
@@ -2,12 +2,70 @@ package exercise2;
 
 public class Patient {
 
-    //TODO: Declare Private Attributes
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    private double weight;
+    private String gender;
 
-    //TODO: Create Constructor
+    public Patient(String firstName, String lastName, int age, double height, double weight, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+    }
 
-    //TODO: Add Getter and Setter Methods
+    public String getFirstName() {
+        return firstName;
+    }
 
-    //TODO: Add toString Method
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String  toString() {
+        return "The Patient is " +  firstName + " " + lastName + " He is " + age + " years old and " + height + " meters tall. His weight is " + weight + " and his gender is " + gender;
+    }
 }
